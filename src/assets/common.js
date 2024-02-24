e8c8f3c4859a609de1dabaae191d305d51a60298
Mitchell <mitchiemt11@gmail.com>
2024-02-24 23:44:40 +0200

    Complete custom splash screen UI

--- src/assets/common.js
+++ src/assets/common.js
@@ -2,4 +2,5 @@
 export const DELI_RED = "#DC2626";
 export const DELI_BLUE= "#18DBE8"
 export const DELI_DARK = "#111214";
+export const DELI_TEXT = "#0D0E0F";
 
--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,13 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { DELI_BLUE, DELI_TEXT } from '../../assets/common'
+
 
 const SplashPage = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
-
-      <Text>[Image here ...]</Text>
-      <Text>MultiDeli</Text>
-      <Text>Multichoice's all in one app</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: DELI_BLUE }}>
+      <Image source={require('../../assets/logo.png')} style={{ width: 220, height: 250 }} />
+      <Text style={{fontSize: 20, color: DELI_TEXT, marginTop: 20, fontWeight: '800'}}>MultiDeli</Text>
+      <Text
+        style={{fontSize: 16, color: DELI_TEXT, marginTop: 20, fontWeight: '400'}}>
+        Multichoice's all in one app
+      </Text>
     </View>
   )
 }
