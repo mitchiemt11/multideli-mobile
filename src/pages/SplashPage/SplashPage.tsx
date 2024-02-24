6ad6ca766e405e9e65af8a9ebd3a5b00077963d1
Mitchell <mitchiemt11@gmail.com>
2024-02-25 01:06:49 +0200

    Hide custom splash with timed navigation

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,20 +1,35 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../../types/types';
 
+interface IPageProps {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Splash'>;
+}
 const CHEF_LOGO = require('../../assets/images/chef_logo.png');
 
-const SplashPage = () => {
+const SplashPage = ({ navigation }: IPageProps) => {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate();
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const navigate = () => {
+    navigation.navigate('Welcome');
+  };
+
   return (
     <View style={styles.container}>
       <Image source={CHEF_LOGO} style={styles.chefLogo} />
       <Text style={styles.title}>MultiDeli</Text>
-      <Text
-        style={styles.description}>
-        Multichoice's all in one app
-      </Text>
+      <Text style={styles.description}>Multichoice's all in one app</Text>
     </View>
-  )
-}
+  );
+};
 
 export default SplashPage
