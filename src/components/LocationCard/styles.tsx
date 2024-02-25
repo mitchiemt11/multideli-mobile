487ce61cb0766aed6fdc8dce02a2bdccc0e605ab
Mitchell <mitchiemt11@gmail.com>
2024-02-25 14:22:39 +0200

    Add complete location card

---
+++ src/components/LocationCard/styles.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+import { DELI_TEXT } from '../../assets/common';
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#222529',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+  },
+  locationIcon: {
+    width: 20,
+    height: 20,
+    marginBottom: 10
+  },
+  locationName: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 22,
+    color: 'white'
+  },
+  locationID: {
+    fontSize: 16,
+    color: '#FFFFFFA3',
+    lineHeight: 22,
+    fontWeight: '400'
+  },
+  Button: {
+    width: '100%',
+    backgroundColor: '#FFFFFF1A',
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#FFFFFF1A',
+    marginTop: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ButtonText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: 'white',
+    lineHeight: 22,
+  },
+});
+
+export default styles;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,16 @@
 import { View, Button, Text, StatusBar } from 'react-native'
 import React from 'react'
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../types/types';
 import styles from './styles';
 import Banner from '../../components/Banner/Banner';
+import LocationCard from '../../components/LocationCard/LocationCard';
 
 interface IPageProps {
   navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
 }
 
-const HomePage = ({navigation}: IPageProps) => {
+const HomePage = ({ navigation }: IPageProps) => {
   return (
     <View style={styles.Container}>
       <StatusBar barStyle="light-content" />
@@ -17,13 +18,14 @@ const HomePage = ({navigation}: IPageProps) => {
         <Text style={styles.Header}>Hi Mitchell</Text>
         <Text style={styles.Reminder}>It's time for lunch :) </Text>
       </View>
-     <Banner/>
-     <View style={styles.Location}>
+      <Banner />
+      <View style={styles.Location}>
         <Text style={styles.distanceHeader}>Nearest fridge</Text>
         <View style={styles.timePill}>
-        <Text style={styles.time}>2 mins of walk </Text>
+          <Text style={styles.time}>2 mins of walk </Text>
         </View>
       </View>
+      <LocationCard/>
       <Button
         title="Go to Settings"
         onPress={() => navigation.navigate('Settings')}
