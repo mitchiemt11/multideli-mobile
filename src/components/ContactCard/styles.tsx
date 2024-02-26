571e78e2c07ddf91b17a45aa1d78e16b5bde7118
Mitchell <mitchiemt11@gmail.com>
2024-02-26 12:01:17 +0200

    Add contact card

--- src/components/ContactCard/styles.tsx
+++ src/components/ContactCard/styles.tsx
@@ -13,23 +13,31 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   left: {
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: 'column',
+    alignItems: 'flex-start'
   },
-  icon: {
+  contact: {
     width: 20,
     height: 20,
     marginRight: 10
   },
   title: {
     fontSize: 16,
-    color: '#FFFFFF'
+    color: '#FFFFFF',
+    fontWeight: '400',
+    lineHeight: 22
+  },
+  details: {
+    color: '#FFFFFFA3',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 20
   },
   right: {
     flex: 1,
     alignItems: 'flex-end'
   },
-  arrow: {
+  pencil: {
     width: 20,
     height: 20
   }
--- src/pages/SetttingsPage/SettingsPage.tsx
+++ src/pages/SetttingsPage/SettingsPage.tsx
@@ -2,6 +2,7 @@ import { View } from 'react-native'
 import React from 'react'
 import SettingsCard from '../../components/SettingsCard/SettingsCard'
 import styles from './styles'
+import ContactCard from '../../components/ContactCard/ContactCard'
 
 const SettingsPage = () => {
   return (
@@ -11,12 +12,22 @@ const SettingsPage = () => {
         icon={require('../../assets/images/money.png')}
         onPress={() => console.log('PaymentMethods')}
       />
-       <SettingsCard
+      <SettingsCard
         title="Personal details"
         icon={require('../../assets/images/personal.png')}
         onPress={() => console.log('Personal Details')}
       />
-       <SettingsCard
+      <ContactCard
+        title="Email"
+        details="mitchell.test@mail.com"
+        onPress={() => console.log('Contact')}
+      />
+      <ContactCard
+        title="Phone Number"
+        details="+27 83 123 4567"
+        onPress={() => console.log('Contact')}
+      />
+      <SettingsCard
         title="Additional"
         icon={require('../../assets/images/additional.png')}
         onPress={() => console.log('Additional')}
