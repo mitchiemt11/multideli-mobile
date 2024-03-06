b68696e61e297acaa194d583d3b8bc66bd4f0070
Mitchell <mitchiemt11@gmail.com>
2024-03-06 12:00:40 +0200

    Code clean up on product details

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -2,7 +2,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useRef } from 'react'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../types/types';
-import { DELI_BLUE, DELI_DARK, DELI_TEXT } from '../../assets/common';
+import { DELI_BLUE, DELI_TEXT } from '../../assets/common';
 import SmallButton from '../../components/SmallButton/SmallButton';
 import BottomSheet, { BottomSheetMethods } from '@devvie/bottom-sheet';
 import HeroButton from '../../components/HeroButton/HeroButton';
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Item } from '../../components/ItemList/ItemList';
 import type { RouteProp } from '@react-navigation/native';
 import { addOrder } from '../../features/addOrder/addOrderSlice';
+import styles from './styles';
 
 interface IPageProps {
   navigation: NativeStackNavigationProp<RootStackParamList, 'Product'>;
@@ -33,51 +34,41 @@ const ProductDetailsPage = ({ navigation, route }: IPageProps) => {
 
   const sheetRef = useRef<BottomSheetMethods>(null)
   return (
-    <View style={{ flex: 1, backgroundColor: DELI_DARK }}>
-      <Image source={require('../../assets/images/product.png')} style={{ width: '100%', height: 320, objectFit: 'cover' }} />
-      <View style={{ padding: 20 }}>
-        <Text style={{ color: 'white', fontSize: 28, lineHeight: 26, fontWeight: '700' }}>{item.name}</Text>
-        <View style={{ flexDirection: 'row', marginTop: 20 }}>
-          <Text style={{ color: 'white', fontSize: 15, fontWeight: '400', lineHeight: 20 }}>
+    <View style={styles.container}>
+      <Image source={require('../../assets/images/product.png')} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.header}>{item.name}</Text>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.description}>
             {item.description}
           </Text>
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-          <View style={{ flexDirection: 'row' }}>
-            <Image source={require('../../assets/images/grams.png')} style={{ width: 16, height: 15, alignSelf: 'center' }} />
-            <Text style={{ color: '#FFFFFFA3', fontSize: 14, fontWeight: '500', lineHeight: 22 }}> {item.grams}g</Text>
+        <View style={styles.infoContainer}>
+          <View style={styles.itemImage}>
+            <Image source={require('../../assets/images/grams.png')} style={styles.gramsImage} />
+            <Text style={styles.measurements}> {item.grams}g</Text>
           </View>
-          <View style={{ flexDirection: 'row' }}>
-            <Image source={require('../../assets/images/fire.png')} style={{ width: 12, height: 17, alignSelf: 'center' }} />
-            <Text style={{ color: '#FFFFFFA3', fontSize: 14, fontWeight: '500', lineHeight: 22 }}> {item.calories} kcal</Text>
+          <View style={styles.itemImage}>
+            <Image source={require('../../assets/images/fire.png')} style={styles.caloriesImage} />
+            <Text style={styles.measurements}> {item.calories} kcal</Text>
 
           </View>
-          <View style={{ flexDirection: 'row' }}>
-            <Image source={require('../../assets/images/info.png')} style={{ width: 16, height: 16, alignSelf: 'center' }} />
-            <Text style={{ color: '#FFFFFFA3', fontSize: 14, fontWeight: '500', lineHeight: 22, marginRight: 8 }}> {item.info}</Text>
+          <View style={styles.itemImage}>
+            <Image source={require('../../assets/images/info.png')} style={styles.infoImage} />
+            <Text style={styles.measurementsLast}> {item.info}</Text>
           </View>
         </View>
         <View>
-          <View
-            style={{
-              backgroundColor: 'red',
-              padding: 10,
-              borderRadius: 5,
-              marginTop: 20,
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'row',
-            }}
-          >
-            <Image source={require('../../assets/images/spicy.png')} style={{ width: 12, height: 17 }} />
-            <Text style={{ color: 'white', fontSize: 16, textAlign: 'center', marginLeft: 5 }}>A bit spicy, but not too much</Text>
+          <View style={styles.bannerContainer}>
+            <Image source={require('../../assets/images/spicy.png')} style={styles.bannerImage} />
+            <Text style={styles.bannerText}>A bit spicy, but not too much</Text>
           </View>
         </View>
-        <View style={{ flexDirection: 'row', marginTop: 20 }}>
-          <Text style={{ color: 'white', fontSize: 26 }}>R{item.price}</Text>
-          <Text style={{ color: '#FFFFFFA3', fontSize: 14, marginLeft: 8, alignSelf: 'center' }}>3 available</Text>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>R{item.price}</Text>
+          <Text style={styles.availability}>3 available</Text>
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 60 }}>
+        <View style={styles.buttonContainer}>
           <SmallButton
             color={DELI_BLUE}
             title='Grab it'
@@ -95,12 +86,12 @@ const ProductDetailsPage = ({ navigation, route }: IPageProps) => {
       <View >
         <BottomSheet
           containerHeight={500}
-          style={{ backgroundColor: '#131516' }}
+          style={styles.bottomSheet}
           ref={sheetRef}>
-          <Text style={{ color: 'white', fontSize: 24, textAlign: 'center', fontWeight: '400', marginTop: 10, lineHeight: 28 }}>
+          <Text style={styles.orderText}>
             You are about to order a
           </Text>
-          <Text style={{ color: 'white', fontSize: 24, textAlign: 'center', fontWeight: '400', lineHeight: 28 }}>
+          <Text style={styles.itemName}>
             {item.name}
           </Text>
           <HeroButton
@@ -112,17 +103,9 @@ const ProductDetailsPage = ({ navigation, route }: IPageProps) => {
             textColor={DELI_TEXT}
           />
           <TouchableOpacity
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              marginTop: 20,
-              backgroundColor: '#131516',
-              width: '100%',
-              height: 50,
-              borderRadius: 25,
-            }}
+            style={styles.backButton}
             onPress={() => sheetRef.current?.close()}>
-            <Text style={{ color: 'white', fontSize: 22, textAlign: 'center', lineHeight: 22 }}>Go back</Text>
+            <Text style={styles.backText}>Go back</Text>
           </TouchableOpacity>
         </BottomSheet>
       </View>
---
+++ src/pages/ProductDetailsPage/styles.tsx
@@ -0,0 +1,143 @@
+import { StyleSheet } from "react-native"; 
+import { DELI_DARK } from "../../assets/common";
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: DELI_DARK
+  },
+  image : {
+    width: '100%',
+    height: 320,
+    objectFit: 'cover'
+  },
+  textContainer: { padding: 20 },
+  header: {
+    color: "white",
+    fontSize: 28,
+    lineHeight: 26,
+    fontWeight: "700",
+  },
+  descriptionContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  description: {
+    color: "white",
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: "400",
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  itemImage: {
+    flexDirection: 'row',
+  },
+  bannerContainer: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  bannerImage: {
+    width: 12,
+    height: 17
+  },
+  bannerText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginLeft: 5
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  price: {
+    color: 'white',
+    fontSize: 26
+  },
+  availability: {
+    color: '#FFFFFFA3',
+    fontSize: 14,
+    marginLeft: 8,
+    alignSelf: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 60
+  },
+  measurements: {
+    color: '#FFFFFFA3',
+    fontSize: 14,
+    lineHeight: 22,
+    fontWeight: "500",
+  },
+  gramsImage: {
+    width: 16,
+    height: 15,
+    alignSelf: 'center'
+  },
+  caloriesImage: {
+    width: 12,
+    height: 17,
+    alignSelf: 'center'
+  },
+  infoImage: {
+    width: 16,
+    height: 16,
+    alignSelf: 'center'
+  },
+  measurementsLast: {
+    color: '#FFFFFFA3',
+    fontSize: 14,
+    lineHeight: 22,
+    fontWeight: "500",
+    marginRight: 8
+
+  },
+  bottomSheet: { 
+    backgroundColor: '#131516'
+  },
+  orderText: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: '400',
+    marginTop: 10,
+    lineHeight: 28
+  },
+  itemName: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: '400',
+    lineHeight: 28
+  },
+  backButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: '#131516',
+    width: '100%',
+    height: 50,
+    borderRadius: 25,
+  },
+  backText: {
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center',
+    lineHeight: 22
+  }
+
+})
+
+export default styles;
